22905a2ce50ebf4a4a655299c1c45ac8acf0a9cc
fojleR <frabbybics10@gmail.com>
2024-10-18 11:26:12 +0600

    new commit

---
+++ tailwind-playlist-lesson-2/tailwind-playlist-lesson-2/tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  mode: 'jit',
+  purge: ['index.html'],
+  darkMode: 'media', // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
